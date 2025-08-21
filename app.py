023dc9dabff22ce6e2d279b17f9ae51b93dba69d
Madan Raj Upadhyay <mmyytubee@gmail.com>
2025-08-21 19:30:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ Cuvette_url = "https://cuvette.tech/jobs/internships"
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return {"message": "Flask app running on Nhost ✅"}
 
 @app.route("/scrape_cuvette", methods=["GET"])
 def scrape_api_cuvette():
@@ -17,4 +20,5 @@ def scrape_api_cuvette():
 
 
 if __name__ == "__main__":
+
     app.run(debug=True, host='0.0.0.0', port=5000)
